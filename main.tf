f2d534a3cc2d971eeb1a244bbc4c9f9cb2b4cec4
italo <italo.pessoa@hotmail.com>
2024-09-17 23:50:37 -0300

    create eks infra

--- main.tf
+++ main.tf
@@ -6,22 +6,22 @@ module "vpc" {
   name    = var.eks_vpc_name
 }
 
-# module "eks" {
-#   source  = "./modules/eks"
-#   region  = var.region
-#   profile = var.profile
+module "eks" {
+  source  = "./modules/eks"
+  region  = var.region
+  profile = var.profile
 
-#   cluster_name    = var.cluster_name
-#   eks_vpc_id      = module.vpc.vpc_id
-#   private_subnets = module.vpc.private_subnets
-# }
+  cluster_name    = var.cluster_name
+  eks_vpc_id      = module.vpc.vpc_id
+  private_subnets = module.vpc.private_subnets
+}
 
-# module "loadbalancer-controller" {
-#   depends_on        = [module.eks]
-#   source            = "./modules/loadbalancer-controller"
-#   oidc_provider_arn = module.eks.oidc_provider_arn
-#   name              = var.nlb_name
-#   cluster_name      = module.eks.cluster_name
-#   region            = var.region
-#   vpc_id            = module.vpc.vpc_id
-# }
+module "loadbalancer-controller" {
+  depends_on        = [module.eks]
+  source            = "./modules/loadbalancer-controller"
+  oidc_provider_arn = module.eks.oidc_provider_arn
+  name              = var.nlb_name
+  cluster_name      = module.eks.cluster_name
+  region            = var.region
+  vpc_id            = module.vpc.vpc_id
+}
